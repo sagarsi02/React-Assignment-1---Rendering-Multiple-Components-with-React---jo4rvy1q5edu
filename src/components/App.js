01a2eb85be078cb779be35209cf10cb2cb176990
Sagar <sagarsi0202@gmail.com>
2022-07-11 12:05:17 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div id="main">
+      <h3 data-ns-test="project-name">Portfolio Website</h3>
+      <p data-ns-test="project-description">When i'm learn Completely React then i will create a our portfolio website this website in completely single page Website</p>
+    </div>
+  )
+}
+
+
+export default App;
